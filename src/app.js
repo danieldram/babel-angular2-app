cc2e5d6b11edb422ba1f5ff3c218a930b78c90e0
Daniel Ram <danieldram@gmail.com>
2016-02-18 22:07:36 -0800

    save and prep for phase 3

--- src/app.js
+++ src/app.js
@@ -11,18 +11,25 @@ import { Component } from 'angular2/core';
     <form class="ui large form segment">
       <h3 class="ui header">Add a Link</h3>
         <div class="field">
-          <label for="title">Title:</label> <input name="title">
+          <label for="title">Title:</label>
+          <input name="title"  #newtitle>
         </div>
         <div class="field">
           <label for="link">Link:</label>
-        <input name="link">
+          <input name="link"  #newlink>
       </div>
+       <button (click)="addArticle(newtitle, newlink)" class="ui positive right floated button">
+        Submit link
+      </button>
     </form> `
 })
 
 class RedditApp {
-  constructor() {
+  constructor() {}
 
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): void {
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
   }
+
 }
 bootstrap(RedditApp);
