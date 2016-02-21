40878d1f5f306563177d977fe213f01c324b868a
Daniel Ram <danieldram@gmail.com>
2016-02-21 12:05:41 -0800

    add abstraction to (click) similar to example in book

--- src/app.js
+++ src/app.js
@@ -5,25 +5,32 @@ import { bootstrap } from 'angular2/platform/browser';
 import { Component } from 'angular2/core';
 
 class hmm{
+  data:string;
+  constructor(){
+    this.data = "fjdkfjkdfjd";
+  }
 
+  change(){
+    this.data = "change it up";
+  }
 }
 
 @Component({
   selector: 'test',
   template: `
       <a href (click)="update()">test</a>
-      <p>{{testing}}</p>
+      <p>{{testing.data}}</p>
       `
 })
 
 class Test{
     testing:string;
     constructor(){
-      this.testing = 'wtf';
+      this.testing = new hmm();
     }
 
     update(){
-      this.testing = 'omg it worked';
+      this.testing.change();
       console.log(this.testing);
       return false;
     }
