b41d900f545d8abfd0f63fcd7643019ad8743bc7
Daniel Ram <danieldram@gmail.com>
2016-02-23 21:33:16 -0800

    new finish

--- src/component-example.js
+++ src/component-example.js
@@ -5,31 +5,31 @@ import 'angular2/bundles/angular2-polyfills';
 import { bootstrap } from 'angular2/platform/browser';
 import { Component} from 'angular2/core';
 
-
-class ExampleTweet{
-  title: String;
-  description: String;
+class ExampleTweets{
+  name:String;
+  description:String;
   constructor({name, description}){
     this.name = name;
     this.description = description;
   }
 }
 
+
 @Component({
-  selector:'tweet',
+  selector:'tweet-this',
   template:`
-  <div>
-    <h1>{{tweets.name}}</h1>
-    <p>{{tweets.description}}<p>
-  </div>
+    <div>
+      <h1>{{tweets.name}}</h1>
+      <p>{{tweets.description}}</p>
+    </div>
   `
 })
 
-class ComponentExample{
-  tweets: ExampleTweet;
+class ExampleTweet{
+  tweets: ExampleTweets;
   constructor(){
-    this.tweets = new ExampleTweet({name:'Kanye West', description:'Mark Zuckerberg... Got a minute? I got a great idea..'});
+    this.tweets = new ExampleTweets({name:"Kanye West", description:"Mark, man you got a minute??"});
   }
 }
 
-bootstrap(ComponentExample);
+bootstrap(ExampleTweet);
